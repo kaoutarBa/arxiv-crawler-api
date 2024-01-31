d72a427ebc926a0144c4ac5f69167a3e7b99364a
kaoutarBa <baldi.kaoutar.ma@gmail.com>
2024-01-31 23:05:15 +0100

    reformat the code with black & isort

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, request, jsonify, send_file, make_response
-from pymongo import MongoClient
-from dotenv import load_dotenv
-from src.metadata_crawler import crawler
 
+from dotenv import load_dotenv
+from flask import Flask, jsonify, make_response, request, send_file
+from pymongo import MongoClient
+
+from src.metadata_crawler import crawler
 
 # Load environment variables from .env
 load_dotenv()
@@ -27,36 +28,41 @@ crawler()
 
 
 # Route to upload a new article
-@app.route('/articles', methods=['POST'])
+@app.route("/articles", methods=["POST"])
 def upload_article():
     try:
         # Extract data from the request
         data = request.json
 
         # Validate that the required fields are present
-        required_fields = ['id', 'title', 'authors', 'categories', 'links', 'published', 'summary', 'updated']
+        required_fields = [
+            "id",
+            "title",
+            "authors",
+            "categories",
+            "links",
+            "published",
+            "summary",
+            "updated",
+        ]
         if not all(field in data for field in required_fields):
-            return jsonify({'error': 'Missing required fields'}), 400
+            return jsonify({"error": "Missing required fields"}), 400
 
         # Insert the metadata into the MongoDB collection
-        entries_collection.update_one(
-            {'id': data['id']},
-            {'$set': data},
-            upsert=True
-        )
+        entries_collection.update_one({"id": data["id"]}, {"$set": data}, upsert=True)
 
-        return jsonify({'id': data['id']}), 201  # Respond with the document ID
+        return jsonify({"id": data["id"]}), 201  # Respond with the document ID
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
 
-@app.route('/articles/', methods=['GET'])
-@app.route('/articles', methods=['GET'])
+@app.route("/articles/", methods=["GET"])
+@app.route("/articles", methods=["GET"])
 def list_articles():
     try:
         # Define default values for page and per_page
-        page = int(request.args.get('page', 1))
-        per_page = int(request.args.get('per_page', 10))
+        page = int(request.args.get("page", 1))
+        per_page = int(request.args.get("per_page", 10))
 
         # Calculate the starting index for pagination
         start_index = (page - 1) * per_page
@@ -65,51 +71,54 @@ def list_articles():
         query = {}
 
         # Filter by category
-        category = request.args.get('category')
+        category = request.args.get("category")
         if category:
-            query['categories'] = category
+            query["categories"] = category
 
         # Filter by authors
-        authors = request.args.get('authors')
+        authors = request.args.get("authors")
         if authors:
-            query['authors'] = authors
+            query["authors"] = authors
         # Fetch articles from the MongoDB collection with pagination and filters
-        articles = list(entries_collection.find(query, {'_id': 0}).skip(start_index).limit(per_page))
+        articles = list(
+            entries_collection.find(query, {"_id": 0}).skip(start_index).limit(per_page)
+        )
 
-        return jsonify({'articles': articles}), 200
+        return jsonify({"articles": articles}), 200
 
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
+
 
 # Route to describe a specific article
-@app.route('/articles/<id>', methods=['GET'])
+@app.route("/articles/<id>", methods=["GET"])
 def describe_article(id):
     try:
         # Fetch the article with the given ID from the MongoDB collection
-        article = entries_collection.find_one({'id': id}, {'_id': 0})
+        article = entries_collection.find_one({"id": id}, {"_id": 0})
 
         if article:
-            return jsonify({'article': article})
-        return jsonify({'error': 'Article not found'}), 404
+            return jsonify({"article": article})
+        return jsonify({"error": "Article not found"}), 404
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/summary/<id>', methods=['GET'])
+
+@app.route("/summary/<id>", methods=["GET"])
 def get_article_summary(id):
     try:
         # Fetch the article from the MongoDB collection
-        article = entries_collection.find_one({'id': id}, {'_id': 0, 'summary': 1})
+        article = entries_collection.find_one({"id": id}, {"_id": 0, "summary": 1})
 
         if article:
             # Return the summary as JSON response
-            return jsonify({'summary': article['summary']})
-        return jsonify({'error': f'Article with ID {id} not found'}), 404
+            return jsonify({"summary": article["summary"]})
+        return jsonify({"error": f"Article with ID {id} not found"}), 404
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-            
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def handle_welcome():
     title = "Welcome !"
     message = "Enjoy interacting with the API in order to get metadata of articles from Arxiv !"
@@ -118,13 +127,15 @@ def handle_welcome():
     response = make_response(content)
     return response
 
+
 @app.errorhandler(404)
 def page_not_found(error):
-    title  = "Oups !"
+    title = "Oups !"
     message = "Apparently, there is an error when trying to retrieve the request data!<br/> Make sure to issue a correct query.<br/> Read the Manual for guidance."
     content = f"<html><head><title>{title}</title></head><body><h1>{title}</h1><p>{message}</p></body></html>"
     response = make_response(content)
     return response
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- src/metadata_crawler.py
+++ src/metadata_crawler.py
@@ -1,23 +1,18 @@
-import requests
-import feedparser
-from pymongo import MongoClient
-
 import os
+
+import feedparser
+import requests
+from pymongo import MongoClient
 
 # MongoDB configuration
 MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME=os.getenv("DB_NAME")
-COLLECTION_NAME=os.getenv("COLLECTION_NAME")
-
+DB_NAME = os.getenv("DB_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
 
 
 def fetch_arxiv_data(start_index, max_results):
-    base_url = 'http://export.arxiv.org/api/query'
-    params = {
-        'search_query': 'all',
-        'start': start_index,
-        'max_results': max_results
-    }
+    base_url = "http://export.arxiv.org/api/query"
+    params = {"search_query": "all", "start": start_index, "max_results": max_results}
 
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
@@ -27,39 +22,47 @@ def fetch_arxiv_data(start_index, max_results):
         print(f"Error fetching data. Status Code: {response.status_code}")
         return None
 
+
 def extract_metadata(entry):
     metadata = {}
 
-    metadata['title'] = entry.title
-    metadata['id'] = entry.id.split('/')[-1]
-    metadata['published'] = entry.published
-    metadata['updated'] = entry.updated
+    metadata["title"] = entry.title
+    metadata["id"] = entry.id.split("/")[-1]
+    metadata["published"] = entry.published
+    metadata["updated"] = entry.updated
 
-    metadata['summary'] = entry.summary
+    metadata["summary"] = entry.summary
 
     # Extracting all authors
-    metadata['authors'] = [author.name for author in entry.authors]
+    metadata["authors"] = [author.name for author in entry.authors]
 
-    metadata['categories'] = [category.term for category in entry.tags]
+    metadata["categories"] = [category.term for category in entry.tags]
 
-    metadata['links'] = [{'rel': link.rel, 'title': link.title, 'href': link.href} if hasattr(link, 'title') else {'rel': link.rel, 'href': link.href} for link in entry.links]
+    metadata["links"] = [
+        (
+            {"rel": link.rel, "title": link.title, "href": link.href}
+            if hasattr(link, "title")
+            else {"rel": link.rel, "href": link.href}
+        )
+        for link in entry.links
+    ]
 
     return metadata
 
+
 def save_to_database(metadata_list):
-    
+
     client = MongoClient(MONGO_URI)
     db = client.get_database(DB_NAME)
     entries_collection = db.get_collection(COLLECTION_NAME)
 
     for metadata in metadata_list:
         entries_collection.update_one(
-            {'id': metadata['id']},
-            {'$set': metadata},
-            upsert=True
+            {"id": metadata["id"]}, {"$set": metadata}, upsert=True
         )
 
     print(f"Metadata for {len(metadata_list)} entries saved to MongoDB.")
+
 
 def crawler():
     start_index = 0
@@ -71,5 +74,6 @@ def crawler():
         metadata_list = [extract_metadata(entry) for entry in arxiv_entries]
         save_to_database(metadata_list)
 
-#if __name__ == "__main__":
-    #main()
+
+# if __name__ == "__main__":
+# main()
